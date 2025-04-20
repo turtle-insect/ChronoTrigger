c8330043ff458b2722a545db6d705729c5d86aaa
turtle-insect <turtle.insect@gmail.com>
2025-04-20 18:16:26 +0900

    refactor

--- ChronoTrigger/Util.cs
+++ ChronoTrigger/Util.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChronoTrigger
+﻿namespace ChronoTrigger
 {
 	internal class Util
 	{
